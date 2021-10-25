b824144189ddde075b9b9fd4885b32347edf0a42
sofMD <xsofiamaria.32@gmail.com>
2021-10-25 15:36:07 +0200

    divisibile per 3 pt1

--- js/master.js
+++ js/master.js
@@ -4,4 +4,8 @@ const boxes = document.querySelector('.boxes');
 for (let box = 1; box <= 100; box++) {
     console.log(box);
     boxes.innerHTML += ` <div class="square">${box}</div>`;
+    // calcolo numeri divisibili per 3
+    if(box % 3 === 0){
+        console.log('div3')
+    }
 }
