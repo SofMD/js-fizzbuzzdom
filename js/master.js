79dd81fdd813ef77c45aa20695af94f1f61014c7
sofMD <xsofiamaria.32@gmail.com>
2021-10-25 15:20:09 +0200

    square

--- js/master.js
+++ js/master.js
@@ -1,7 +1,7 @@
 // generazione ciclo da 1 a 100
 const boxes = document.querySelector('.boxes');
-let size = 7;
 
 for (let box = 1; box <= 100; box++) {
     console.log(box);
+    boxes.innerHTML += ` <div class="square">${box}</div>`;
 }
