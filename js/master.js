119adeb402ab2525f3e5b9d7cf953e299f2eabce
sofMD <xsofiamaria.32@gmail.com>
2021-10-25 16:12:38 +0200

    struttura if numeri divisibili

--- js/master.js
+++ js/master.js
@@ -1,11 +1,19 @@
 // generazione ciclo da 1 a 100
-const boxes = document.querySelector('.boxes');
+let boxes = document.querySelector('.boxes');
 
 for (let box = 1; box <= 100; box++) {
     console.log(box);
-    boxes.innerHTML += ` <div class="square">${box}</div>`;
-    // calcolo numeri divisibili per 3
+    boxes.innerHTML += ` <div class="square box-${box}">${box}</div>`;
+
+    // calcolo numeri divisibili per 3 e 5
+
     if(box % 3 === 0){
-        console.log('div3')
+     
+    }
+    else if(box % 3 === 0){
+
+    }
+    else if (box % 3 === 0) && (box % 3 === 0){
+        
     }
 }
