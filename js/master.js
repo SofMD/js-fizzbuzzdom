d4289e614c49f019c73b13728f3861c03db84cd9
sofMD <xsofiamaria.32@gmail.com>
2021-10-25 14:56:20 +0200

    gen-ciclo1/100 pt2

--- js/master.js
+++ js/master.js
@@ -1,3 +1,7 @@
 // generazione ciclo da 1 a 100
-const boxes = document.querySelector('.boxes')
+const boxes = document.querySelector('.boxes');
+let size = 7;
 
+for (let row = 1; row <= size; row++) {
+    console.log(row);
+}
