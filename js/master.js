dddacce8a9761a49f8f5582b7f85426861ef7588
sofMD <xsofiamaria.32@gmail.com>
2021-10-25 17:04:52 +0200

    fine

--- js/master.js
+++ js/master.js
@@ -1,19 +1,25 @@
 // generazione ciclo da 1 a 100
 let boxes = document.querySelector('.boxes');
 
+// box diversi
 for (let box = 1; box <= 100; box++) {
-    console.log(box);
-    boxes.innerHTML += ` <div class="square box-${box}">${box}</div>`;
 
-    // calcolo numeri divisibili per 3 e 5
-
-    if(box % 3 === 0){
-     
+    if((box % 3 === 0)&&(box % 5 === 0)){
+        boxes.innerHTML += ` <div class="square box-3-5">fizzbuzz</div>`;
     }
     else if(box % 3 === 0){
-
+        boxes.innerHTML += ` <div class="square box-3">fizz</div>`;
     }
-    else if (box % 3 === 0) && (box % 3 === 0){
-        
+    else if (box % 5 === 0){
+        boxes.innerHTML += ` <div class="square box-5">buzz</div>`;
     }
+    else {
+        boxes.innerHTML += ` <div class="square">${box}</div>`;
+    }
+    
 }
+
+
+
+
+
